769c0026805ddea2277c0f12510b0b06930af740
one-atom <519001901@qq.com>
2017-10-22 16:58:18 -0400

    HW4

---
+++ HW4-Segmentation/test.py
@@ -0,0 +1,15 @@
+import sys
+import os
+import cv2
+
+
+if __name__ == '__main__':
+	root = os.path.dirname(os.path.realpath(__file__))
+	print root
+	os.system("python "+root+"/Source/main.py "+root+"/TestData/astronaut.png "+root + "/TestData/astronaut_marking.png "+root+"/Results/")
+
+	import imp
+	RMSD = imp.load_source('main', root + '/Source/main.py')
+	example_output = cv2.imread(root + "/example_output.png", cv2.IMREAD_GRAYSCALE)
+	output = cv2.imread(root + "/Results/mask.png", cv2.IMREAD_GRAYSCALE)
+	print "Error:", RMSD.RMSD(example_output, output)
